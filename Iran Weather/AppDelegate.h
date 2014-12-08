7a097d7540d3077ac8c7af4d41584d3bbea60dd5
aDb <adb@ADBs-MacBook-Pro.local>
2014-12-08 20:27:25 +0330

    Initial Commit

---
+++ Iran Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Iran Weather
+//
+//  Created by aDb on 12/8/14.
+//  Copyright (c) 2014 aDb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Iran Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Iran Weather
+//
+//  Created by aDb on 12/8/14.
+//  Copyright (c) 2014 aDb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
