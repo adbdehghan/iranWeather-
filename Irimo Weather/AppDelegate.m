b605e9768029d49fa93eb1b7e655be95c1baa1af
aDb <adb@ADBs-MacBook-Pro.local>
2015-03-26 10:27:08 +0430

    airplane mode bug fix

--- Irimo Weather/AppDelegate.m
+++ Irimo Weather/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "MainViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+//    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
     
 
     return YES;
--- Irimo Weather/LaunchScreen.m
+++ Irimo Weather/LaunchScreen.m
@@ -33,34 +33,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //  Initialize the blurred overlay view
-    self.blurredOverlayView = [[UIImageView alloc]initWithImage:[[UIImage alloc]init]];
-    self.blurredOverlayView.alpha = 1.0;
-    [self.blurredOverlayView setFrame:self.view.bounds];
-    
     Gradient *gr = [[Gradient alloc]init];
     UIImageView *uv = [[UIImageView alloc]initWithImage:[gr CreateGradient:self.view.frame.size.width Height:self.view.frame.size.height]];
     [self.view addSubview:uv];
-    
+
     
     self.ribbon = [[UIView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height-130 , self.view.bounds.size.width, 80)];
     [self.ribbon setBackgroundColor:[UIColor colorWithWhite:1.0 alpha:0.25]];
     [self.view addSubview:self.ribbon];
 
-    MKPersianFont *title = [[MKPersianFont alloc]init];
-    [title setPersianFont:@"koodak" withText:@"سازمان هواشناسی کشور" fontSize:35 textAlignment:CENTER textWrapped:YES fontColor:[UIColor whiteColor]];
-    [title setFrame:CGRectMake(0,27, self.ribbon.frame.size.width ,self.ribbon.frame.size.height)];
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.ribbon.frame.size.width ,self.ribbon.frame.size.height)];
+    [title setText:@"سازمان هواشناسی کشور"];
+    
+    [title setAdjustsFontSizeToFitWidth:YES];
+    [title setFont:[UIFont fontWithName:@"B Koodak" size:35]];
+    [title setTextAlignment:NSTextAlignmentCenter];
+    [title setTextColor:[UIColor whiteColor]];
+
     
     [self.ribbon addSubview:title];
     
     [self AddSphere];
     
-   // [DBManager deleteRow:@""];
-    
-//   if (![DBManager isTableExist:@"setting_table"])
-//    {
-//        [DBManager createTable];
-//    }
 
     
     timer = [NSTimer scheduledTimerWithTimeInterval:1.6
@@ -73,13 +68,12 @@
 
 -(void)TimerCalled:(NSTimer *)timer
 {
-//    setting = [DBManager selectSetting];
-
-        [self performSegueWithIdentifier:@"LaunchNext" sender:self];
-
+    [self performSegueWithIdentifier:@"LaunchNext" sender:self];
 }
 
-- (void)AddSphere {
+- (void)AddSphere
+{
+    
     sphereView = [[ZYQSphereView alloc] initWithFrame:CGRectMake(10, 60, self.view.frame.size.width-80, self.view.frame.size.width-80)];
     sphereView.center=CGPointMake(self.view.center.x, self.view.center.y-30);
     NSMutableArray *views = [[NSMutableArray alloc] init];
@@ -118,31 +112,6 @@
     [sphereView timerStart];
 }
 
-- (void)setBlurredOverlayImage
-{
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^ {
-        
-        //  Take a screen shot of this controller's view
-        UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, NO, 0.0);
-        CGContextRef context = UIGraphicsGetCurrentContext();
-        
-        [self.view.layer renderInContext:context];
-        UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
-        
-        UIGraphicsEndImageContext();
-        
-        //  Blur the screen shot
-        UIImage *blurred = [image applyBlurWithRadius:20
-                                            tintColor:[UIColor colorWithWhite:0.15 alpha:0.5]
-                                saturationDeltaFactor:1.5
-                                            maskImage:nil];
-        
-        dispatch_async(dispatch_get_main_queue(), ^ {
-            //  Set the blurred overlay view's image with the blurred screenshot
-            self.blurredOverlayView.image = blurred;
-        });
-    });
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -154,14 +123,9 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    [self setBlurredOverlayImage];
-    if (![segue.identifier isEqual:@"LaunchNext"])
-    {
-        AddLocationViewController *detination = [segue destinationViewController];
-        detination.blurredOverlayView = self.blurredOverlayView;
-    }
+
     MainViewController *destination = [segue destinationViewController];
-    destination.setting = setting;
+
 }
 
 
